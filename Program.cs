246a193b8fea31152f74b968ebe01b705f7599dc
marcos041220 <ramosmarkinhu@outlook.com>
2020-03-13 21:15:36 -0300

    exibe nome completo

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace TRABALHO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("exibir nome completo...");
+            Console.Beep();
+            Console.ReadLine();
+            Console.WriteLine("olá usuário:");
+            string nome = "Marcos Vinícius ";
+            string sobrenome = "Dias Ramos";
+            string nomeCompleto = nome + " " + sobrenome;
+            Console.WriteLine(nome  + sobrenome);
+            Console.Beep();
+            Console.ReadLine();
+        }
+    }
+}
